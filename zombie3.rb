e3cbdc6f77238a09bf94594376f0b49d61ae4005
Adriana Agudelo-Toro <adriana.agudelo@nrg-edge.com>
2017-02-17 19:06:27 -0500

    Practica de arrays - Punto 3

---
+++ zombie3.rb
@@ -0,0 +1,9 @@
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "Tactical 10mm"]
+
+def supplies(zombie_apocalypse_supplies, string)
+
+  zombie_apocalypse_supplies.include?(string)
+
+end
+
+p supplies(zombie_apocalypse_supplies, 'boots')

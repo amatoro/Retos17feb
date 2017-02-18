d693396e42b5f9c507ab470d3a735ef21e9bd367
Adriana Agudelo-Toro <adriana.agudelo@nrg-edge.com>
2017-02-17 19:55:08 -0500

    Practica de arrays - Punto 4

---
+++ zombie4.rb
@@ -0,0 +1,13 @@
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "Tactical 10mm"]
+
+
+def single_word?(word)
+  word.strip.include? " "
+end
+
+def carrying_supplies(zombie_apocalypse_supplies)
+  zombie_apocalypse_supplies.delete_if { |supply| single_word?(supply) }
+end
+
+
+p carrying_supplies(zombie_apocalypse_supplies)

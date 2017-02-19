d7d85f618604cded7c7fb29a8311c1e4d92a0e2e
Adriana Agudelo-Toro <adriana.agudelo@nrg-edge.com>
2017-02-18 22:01:23 -0500

    Practica de hashes - Punto 4

---
+++ hashes4.rb
@@ -0,0 +1,28 @@
+extinct_animals = {
+  "Tasmanian Tiger": 1936,
+  "Eastern Hare Wallaby": 1890,
+  "Dodo": 1662,
+  "Pyrenean Ibex": 2000,
+  "Passenger Pigeon": 1914,
+  "West African Black Rhinoceros": 2011,
+  "Laysan Crake": 1923
+}
+
+def is_extinct(extinct_animals, animal)
+  
+  if extinct_animals.has_key?(animal)
+    "The #{animal} extinct :("
+  else
+    "The #{animal} is not extinct!"
+  end
+
+  #extinct_animals.select {|key, value| key == animal }
+
+end
+
+#p extinct_animals
+puts is_extinct(extinct_animals, :'Andean Cat')
+puts is_extinct(extinct_animals, :'Dodo')
+puts is_extinct(extinct_animals, :'Tasmanian Tiger')
+puts is_extinct(extinct_animals, :'Saiga Antelope')
+
